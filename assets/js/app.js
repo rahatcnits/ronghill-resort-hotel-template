4b5224568cb3afa8b374ad438a8b3b690306014d
Rahat CNITS <rahatcnits@gmail.com>
2023-11-01 11:45:33 +0600

    reservation ongoing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 // select box
-const select = document.querySelector(".select");
-const optionsList = document.querySelector(".options_list");
-const options = document.querySelectorAll(".option");
+const select = document.querySelector(".select_room");
+const selectItem = document.querySelector(".room_select_text");
+const optionsList = document.querySelector(".room_option_list");
+const options = document.querySelectorAll(".room_option");
 
 select.addEventListener("click", () => {
   optionsList.classList.toggle("active");
@@ -14,9 +15,33 @@ options.forEach((option) => {
     options.forEach((option) => {
       option.classList.remove("active");
     });
-    select.querySelector("span").innerHTML = option.innerHTML;
+    selectItem.querySelector("span").innerHTML = option.innerHTML;
     option.classList.add("active");
     optionsList.classList.toggle("active");
     select.querySelector(".fa-angle-down").classList.toggle("fa-angle-up");
+  });
+});
+
+// select box
+const childSelect = document.querySelector(".select_child");
+const childSelectItem = document.querySelector(".child_select_text");
+const childOptionsList = document.querySelector(".child_options_list");
+const childOptions = document.querySelectorAll(".child_option");
+
+childSelect.addEventListener("click", () => {
+  childOptionsList.classList.toggle("active");
+  childSelect.querySelector(".fa-angle-down").classList.toggle("fa-angle-up");
+});
+
+// select option
+childOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    childOptions.forEach((option) => {
+      option.classList.remove("active");
+    });
+    childSelectItem.querySelector("span").innerHTML = option.innerHTML;
+    option.classList.add("active");
+    childOptionsList.classList.toggle("active");
+    childSelect.querySelector(".fa-angle-down").classList.toggle("fa-angle-up");
   });
 });
