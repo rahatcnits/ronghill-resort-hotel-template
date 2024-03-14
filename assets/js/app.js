25b7bf1ef7c5b7cc5b17d0b656162a02a9a0d2ed
rahatcnits <rahatcnits@gmail.com>
2024-03-14 13:48:58 +0600

    gallery page ongoing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,66 +1,60 @@
 // select box
-const select = document.querySelector(".select_room");
-const selectItem = document.querySelector(".room_select_text");
-const optionsList = document.querySelector(".room_option_list");
-const options = document.querySelectorAll(".room_option");
-
-select.addEventListener("click", () => {
-  optionsList.classList.toggle("active");
-  select.querySelector(".fa-angle-down").classList.toggle("fa-angle-up");
-});
-
-// select option
-options.forEach((option) => {
-  option.addEventListener("click", () => {
-    options.forEach((option) => {
-      option.classList.remove("active");
+$(".custom-select").each(function () {
+  var classes = $(this).attr("class"),
+    id = $(this).attr("id"),
+    name = $(this).attr("name");
+  var template = '<div class="' + classes + '">';
+  template +=
+    '<span class="custom-select-trigger">' +
+    $(this).attr("placeholder") +
+    "</span>";
+  template += '<div class="custom-options">';
+  $(this)
+    .find("option")
+    .each(function () {
+      template +=
+        '<span class="custom-option ' +
+        $(this).attr("class") +
+        '" data-value="' +
+        $(this).attr("value") +
+        '">' +
+        $(this).html() +
+        "</span>";
     });
-    selectItem.querySelector("span").innerHTML = option.innerHTML;
-    option.classList.add("active");
-    optionsList.classList.toggle("active");
-    select.querySelector(".fa-angle-down").classList.toggle("fa-angle-up");
+  template += "</div></div>";
+
+  $(this).wrap('<div class="custom-select-wrapper"></div>');
+  $(this).hide();
+  $(this).after(template);
+});
+$(".custom-option:first-of-type").hover(
+  function () {
+    $(this).parents(".custom-options").addClass("option-hover");
+  },
+  function () {
+    $(this).parents(".custom-options").removeClass("option-hover");
+  }
+);
+$(".custom-select-trigger").on("click", function () {
+  $("html").one("click", function () {
+    $(".custom-select").removeClass("opened");
   });
+  $(this).parents(".custom-select").toggleClass("opened");
+  event.stopPropagation();
 });
-
-// select box
-const childSelect = document.querySelector(".select_child");
-const childSelectItem = document.querySelector(".child_select_text");
-const childOptionsList = document.querySelector(".child_options_list");
-const childOptions = document.querySelectorAll(".child_option");
-
-childSelect.addEventListener("click", () => {
-  childOptionsList.classList.toggle("active");
-  childSelect.querySelector(".fa-angle-down").classList.toggle("fa-angle-up");
-});
-
-// select option
-childOptions.forEach((option) => {
-  option.addEventListener("click", () => {
-    childOptions.forEach((option) => {
-      option.classList.remove("active");
-    });
-    childSelectItem.querySelector("span").innerHTML = option.innerHTML;
-    option.classList.add("active");
-    childOptionsList.classList.toggle("active");
-    childSelect.querySelector(".fa-angle-down").classList.toggle("fa-angle-up");
-  });
-});
-
-// Counters
-const counterNumDisplays = document.querySelectorAll("#counter_text");
-let countInterval = 4000;
-
-counterNumDisplays.forEach((counterNumDisplay) => {
-  let startValue = 0;
-  let endValue = parseInt(counterNumDisplay.getAttribute("data-val"));
-  let duration = Math.floor(countInterval / endValue);
-  let counter = setInterval(function () {
-    startValue += 1;
-    counterNumDisplay.textContent = startValue + "+";
-    if (startValue === endValue) {
-      clearInterval(counter);
-    }
-  }, duration);
-
-  console.log(endValue);
+$(".custom-option").on("click", function () {
+  $(this)
+    .parents(".custom-select-wrapper")
+    .find("select")
+    .val($(this).data("value"));
+  $(this)
+    .parents(".custom-options")
+    .find(".custom-option")
+    .removeClass("selection");
+  $(this).addClass("selection");
+  $(this).parents(".custom-select").removeClass("opened");
+  $(this)
+    .parents(".custom-select")
+    .find(".custom-select-trigger")
+    .text($(this).text());
 });
